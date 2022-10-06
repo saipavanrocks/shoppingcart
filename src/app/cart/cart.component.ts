ea454e9d77249959a7e4bf04573ffb4b77365d35
saipavanrocks <113990629+saipavanrocks@users.noreply.github.com>
2022-10-06 18:05:04 +0530

    Add files via upload

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,3 +1,4 @@
+import { isNgTemplate } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 import { CartServiceService } from '../service/cart-service.service';
 
@@ -7,28 +8,67 @@ import { CartServiceService } from '../service/cart-service.service';
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent implements OnInit {
+
+  totalitems:any = 1;
+
+
+
   //first we have to store all products
   public products:any = [];
-  public grandTotal:number = 0;
+  //used for printing total price
+  public grandTotal:any;
   constructor(private cartservice:CartServiceService) { }
 
   ngOnInit(): void {
     this.cartservice.getproduct()
     .subscribe(res=>{
       this.products = res;
+
       //for printing total price we are calling this method
       this.grandTotal = this.cartservice.gettotalprice();
 
     })
+
+
   }
 
+  //deleting products
   deleteitem(item:any){
     this.cartservice.removeitem(item);
 
   }
 
+  //Removing entire cart items
   emptycart(){
     this.cartservice.removeallcart();
   }
+
+  //for increasing quantity os products
+  incr(item:any){
+    if(item.quantity != 7){
+      item.quantity =  item.quantity + 1
+    }
+    else{
+      alert("we contain only this many products.Sorry for inconvience")
+    }
+    this.grandTotal = this.cartservice.gettotalprice();
+
+  }
+
+  //For decreasing quantity of products
+  decr(item:any){
+
+    if(item.quantity!=0){
+      item.quantity =  item.quantity - 1
+    }
+    else{
+      alert("You should have atleast one product");
+   }
+
+   this.grandTotal = this.cartservice.gettotalprice();
+
+  }
+
+
 
 }
