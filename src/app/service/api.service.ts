e4270c185d76d0f7cfb4084d3eed9e4d1d4c7e66
saipavanrocks <113990629+saipavanrocks@users.noreply.github.com>
2022-10-12 00:23:51 +0530

    Add files via upload

--- src/app/service/api.service.ts
+++ src/app/service/api.service.ts
@@ -1,21 +1,50 @@
-import { Injectable } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators'
+import { Observable } from 'rxjs';
+import { Iuserdetails } from '../modals/userdetails';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor(private api:HttpClient) { }
+  constructor(private http:HttpClient) { }
+
+
+  addproduct(productdata:any){
+    return this.http.post("https://angularbyprocademy-5761b-default-rtdb.firebaseio.com/products.json", productdata)
+  }
 
   getproducts(){
 
-    return this.api.get("https://angularbyprocademy-5761b-default-rtdb.firebaseio.com/products.json")
+    return this.http.get<any>("https://shoppingcart-dc827-default-rtdb.firebaseio.com/products.json")
     .pipe(map((res:any)=>{
       return res
 
     }))
   }
+
+  API_KEY = 'AIzaSyCLtianNUJiE1jc0YoY4AK83PaK9OvE94A';
+
+  userdetails:Iuserdetails | null = null;
+  loggedInEvent = new EventEmitter<boolean>();
+
+  //Signin
+  signin(email:string, password:string):Observable<Iuserdetails>{
+    return this.http.post<Iuserdetails>(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${this.API_KEY}`,
+    {email, password, returnSecureToken:true});
+
+  }
+
+
+  //signup
+  signup(email:string, password:string){
+    return this.http.post(`https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${this.API_KEY}`,
+    {email, password, returnSecureToken:true})
+
+  }
+
+
 
 }
